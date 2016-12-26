09475e1e5b85b26d2711229f072f3391c8c3dbe7
David Zukowski <zukoweb@gmail.com>
2016-12-25 22:22:23 -0500

    hotfix(utils): temporarily fix npm package resolution

--- utils/paths.util.js
+++ utils/paths.util.js
@@ -1,12 +1,37 @@
+const fs = require('fs')
 const path = require('path')
+
+// pathExists : String -> Boolean
+const pathExists = target => {
+  try {
+    fs.accessSync(target)
+    return true
+  } catch (e) {
+    return false
+  }
+}
 
 // resolveLocalPath : String -> String
 const resolveLocalPath = target =>
   path.resolve(__dirname, '../' + target)
 
 // resolveLocalDependencyPath : String -> String
-const resolveLocalDependencyPath = target =>
-  resolveLocalPath('node_modules/' + target)
+// TODO: this is too naive
+const resolveLocalDependencyPath = target => {
+  const [filepath, query = ''] = target.split('?')
+  const localNodeModulePath = resolveLocalPath('node_modules/' + filepath)
+  if (pathExists(localNodeModulePath)) {
+    return localNodeModulePath + query
+  }
+  const parentNodeModulePath = resolveLocalPath('../' + filepath)
+  if (pathExists(parentNodeModulePath)) {
+    return parentNodeModulePath + query
+  }
+  throw new Error(
+    'Could not locate following dependency in the file system: ' +
+    target
+  )
+}
 
 exports.resolveLocalPath = resolveLocalPath
 exports.resolveLocalDependencyPath = resolveLocalDependencyPath
