eeb9691e12c3ed2806de607af152978d95a44903
David Zukowski <zukoweb@gmail.com>
2017-05-11 08:14:20 -0500

    refactor(web-app): split dev-server and dev-middleware modules

---
+++ src/compilers/web-app/webpack/create-dev-middleware.ts
@@ -0,0 +1,43 @@
+import * as webpack from 'webpack'
+import * as webpackDevMiddleware from 'webpack-dev-middleware'
+import * as webpackHotMiddleware from 'webpack-hot-middleware'
+import { findGenesisDependency } from '../../../utils/paths'
+
+export type Middleware = any
+export type CreateDevMiddlewareOpts = {
+  protocol: string,
+  host: string,
+  port: number,
+  contentBase: string,
+  onCompilerStart?: (stats: any) => any,
+  onCompilerFinish?: (stats: any) => any,
+}
+const createDevMiddleware = (webpackConfig: any, opts: CreateDevMiddlewareOpts): Array<Middleware> => {
+  webpackConfig.output.publicPath = `${opts.protocol}://${opts.host}:${opts.port}/`
+    webpackConfig.entry.main.push(
+      findGenesisDependency('webpack-hot-middleware/client.js') +
+      `?path=${webpackConfig.output.publicPath}__webpack_hmr`
+    )
+    webpackConfig.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin()
+    )
+
+  const webpackCompiler = webpack(webpackConfig)
+  const devMiddleware = webpackDevMiddleware(webpackCompiler, {
+    lazy: false,
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    stats: 'none',
+  })
+  const hotMiddleware = webpackHotMiddleware(webpackCompiler, {
+    log: undefined,
+  })
+
+  const middleware = [devMiddleware, hotMiddleware]
+  if (opts.onCompilerStart) webpackCompiler.plugin('compile', opts.onCompilerStart)
+  if (opts.onCompilerFinish) webpackCompiler.plugin('done', opts.onCompilerFinish)
+  return middleware
+}
+
+export default createDevMiddleware
--- src/compilers/web-app/webpack/create-dev-server.ts
+++ src/compilers/web-app/webpack/create-dev-server.ts
@@ -1,68 +1,10 @@
 import { ICompilerConfig } from '../../../lib/compiler'
 import * as path from 'path'
 import * as express from 'express'
-import * as webpack from 'webpack'
 import * as chalk from 'chalk'
-import * as webpackDevMiddleware from 'webpack-dev-middleware'
-import * as webpackHotMiddleware from 'webpack-hot-middleware'
 import createWebpackConfig from './create-config'
-import { findGenesisDependency } from '../../../utils/paths'
+import createDevMiddleware, { CreateDevMiddlewareOpts } from './create-dev-middleware'
 import * as logger from '../../../utils/logger'
-
-const onCompilerRestart = () => {
-  logger.info('Change detected, starting compiler...')
-}
-const onCompilerDone = (stats: any) => {
-  const hasErrors = stats.hasErrors()
-  const hasWarnings = stats.hasWarnings()
-
-  if (hasErrors) {
-    logger.error(stats.toString('errors-only'))
-    logger.error('Compilation failed, see errors above.')
-  } else if (hasWarnings) {
-    logger.warn(stats.toString('errors-only'))
-    logger.warn('Compilation finished with warnings, see above.')
-  } else {
-    const buildTime = (stats.endTime - stats.startTime)
-    logger.success(`Compilation succeeded in ${buildTime}ms`)
-  }
-}
-
-export type Middleware = any
-export type CreateDevMiddlewareOpts = {
-  protocol: string,
-  host: string,
-  port: number,
-  contentBase: string,
-  onCompile: (stats: any) => any,
-}
-export const createDevMiddleware = (webpackConfig: any, opts: CreateDevMiddlewareOpts): Array<Middleware> => {
-  webpackConfig.output.publicPath = `${opts.protocol}://${opts.host}:${opts.port}/`
-    webpackConfig.entry.main.push(
-      findGenesisDependency('webpack-hot-middleware/client.js') +
-      `?path=${webpackConfig.output.publicPath}__webpack_hmr`
-    )
-    webpackConfig.plugins.push(
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NamedModulesPlugin()
-    )
-
-  const webpackCompiler = webpack(webpackConfig)
-  const devMiddleware = webpackDevMiddleware(webpackCompiler, {
-    lazy: false,
-    noInfo: true,
-    publicPath: webpackConfig.output.publicPath,
-    stats: 'none',
-  })
-  const hotMiddleware = webpackHotMiddleware(webpackCompiler, {
-    log: undefined,
-  })
-
-  const middleware = [devMiddleware, hotMiddleware]
-  webpackCompiler.plugin('done', onCompilerDone)
-  webpackCompiler.plugin('compile', onCompilerRestart)
-  return middleware
-}
 
 export interface DevServerOpts {
   protocol: string,
@@ -73,33 +15,18 @@ class DevServer {
   _server: any
 
   constructor (config: ICompilerConfig, overrides?: Partial<DevServerOpts>) {
-    const opts = Object.assign({
+    const defaults: CreateDevMiddlewareOpts = {
       protocol: 'http',
       host: 'localhost',
       port: 3000,
       contentBase: path.resolve(config.basePath, config.srcDir),
-    }, overrides)
+      onCompilerStart: this._onCompileStart.bind(this),
+    }
+    const opts: CreateDevMiddlewareOpts = Object.assign(defaults, overrides)
 
     this._server = express()
     this._server.use(require('connect-history-api-fallback')())
-    this._server.use(createDevMiddleware(createWebpackConfig(config), {
-      ...opts,
-      onCompile: (stats) => {
-        const hasErrors = stats.hasErrors()
-        const hasWarnings = stats.hasWarnings()
-
-        if (hasErrors) {
-          logger.error(stats.toString('errors-only'))
-          logger.error('Compilation failed, see errors above.')
-        } else if (hasWarnings) {
-          logger.warn(stats.toString('errors-only'))
-          logger.warn('Compilation finished with warnings, see above.')
-        } else {
-          const buildTime = (stats.endTime - stats.startTime)
-          logger.success(`Compilation succeeded in ${buildTime}ms`)
-        }
-      }
-    }))
+    this._server.use(createDevMiddleware(createWebpackConfig(config), opts))
     this._server.use(express.static(path.resolve(config.basePath, 'public')))
     this._server.start = () => new Promise(resolve => {
       this._server.listen(opts.port, () => {
@@ -107,6 +34,22 @@ class DevServer {
         resolve()
       })
     })
+  }
+
+  _onCompileStart (stats: any) {
+    const hasErrors = stats.hasErrors()
+    const hasWarnings = stats.hasWarnings()
+
+    if (hasErrors) {
+      logger.error(stats.toString('errors-only'))
+      logger.error('Compilation failed, see errors above.')
+    } else if (hasWarnings) {
+      logger.warn(stats.toString('errors-only'))
+      logger.warn('Compilation finished with warnings, see above.')
+    } else {
+      const buildTime = (stats.endTime - stats.startTime)
+      logger.success(`Compilation succeeded in ${buildTime}ms`)
+    }
   }
 
   start () {
