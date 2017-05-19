9334f8137085ac2c82b656e06e98e39d7305f490
David Zukowski <zukoweb@gmail.com>
2017-05-19 10:11:38 -0500

    feat(logger): display timestamp with messages

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,22 +12,34 @@ import * as figures from './figures'
   }
 })()
 
+export const timestamp = () => {
+  const timestamp = new Date().toLocaleDateString(undefined, {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  })
+  return `[${timestamp.split(', ')[1]}]`
+}
+
 export const log = (...messages: Array<any>) => {
-  console.log.apply(console, messages)
+  console.log.apply(console, [' ', ...messages])
 }
 
 export const info = (...messages: Array<any>) => {
-  console.info(chalk.cyan(figures.info, ...messages))
+  console.info(chalk.cyan(figures.info, timestamp(), ...messages))
 }
 
 export const success = (...messages: Array<any>) => {
-  log(chalk.green(figures.tick, ...messages))
+  console.log(chalk.green(figures.tick, timestamp(), ...messages))
 }
 
 export const warn = (...messages: Array<any>) => {
-  console.warn(chalk.yellow(figures.warning, ...messages))
+  console.warn(chalk.yellow(figures.warning, timestamp(), ...messages))
 }
 
 export const error = (...messages: Array<any>) => {
-  console.warn(chalk.red(figures.cross, ...messages))
+  console.warn(chalk.red(figures.cross, timestamp(), ...messages))
 }
