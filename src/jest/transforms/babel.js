0b67cfcad233a5bd75785d827ff7a39e0a3189a4
David Zukowski <zukoweb@gmail.com>
2017-09-12 11:06:03 -0500

    fix(jest): fix bad babel transform reference

--- src/jest/transforms/babel.js
+++ src/jest/transforms/babel.js
@@ -4,6 +4,6 @@ const { resolveGenesisDependency } = require('../../utils/fs')
 const config = require('../../babel/create-config')()
 
 config.babelrc = false
-config.plugins.push(resolveGenesisDependency('babel-transform-es2015-modules-commonjs'))
+config.plugins.push(resolveGenesisDependency('babel-plugin-transform-es2015-modules-commonjs'))
 
 module.exports = babelJest.createTransformer(config)
